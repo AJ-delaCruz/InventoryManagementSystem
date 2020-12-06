4e5c73b82f84db9320160c6942cf8f5f6a1378f3
JesusM1997 <70827842+JesusM1997@users.noreply.github.com>
2020-12-05 23:05:53 -0800

    Update AddProductMessage.java

--- AddProductMessage.java
+++ AddProductMessage.java
@@ -1,10 +1,4 @@
-package Controller;
-
-/**
- * Class AddProductMessage
- * Message of adding product
- */
-public class AddProductMessage implements Message {
+{
     private String name;
     private String category;
     private double price;
