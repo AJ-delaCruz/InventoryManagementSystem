ab003b710eb9c6d317856a46013395fcaff4b7d2
JesusM1997 <70827842+JesusM1997@users.noreply.github.com>
2020-12-05 23:20:38 -0800

    Update ProductList.java

--- src/View/ProductList.java
+++ src/View/ProductList.java
@@ -1,18 +1,24 @@
 package View;
 
 import Model.LineProduct;
-
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
-//list of products added
+/**
+ * Class ProductList
+ * Implements Iterator Pattern.
+ * Shows list of Products added
+ */
 public class ProductList {
     private ArrayList<LineProduct> products;
     private ArrayList<ChangeListener> listeners;
 
+    /**
+     * Constructor for the ProductList
+     * Class.
+     */
     public ProductList() {
         products = new ArrayList<>();
         listeners = new ArrayList<>();
@@ -20,7 +26,6 @@ public class ProductList {
 
     /**
      * Adds a product to the product list.
-     *
      * @param product the product to add
      */
     public void addProduct(LineProduct product) {
@@ -33,7 +38,6 @@ public class ProductList {
 
     /**
      * Adds a change listener to the Product list.
-     *
      * @param listener the change listener to add
      */
     public void addChangeListener(ChangeListener listener) {
@@ -41,17 +45,16 @@ public class ProductList {
     }
 
     /**
+     * Helper method.
      * Gets an iterator that iterates through the products.
-     *
      * @return an iterator for the products
      */
-    public Iterator<LineProduct> getProducts() {
+    private Iterator<LineProduct> getItems() {
         return new
                 Iterator<>() {
                     public boolean hasNext() {
                         return current < products.size();
                     }
-
                     public LineProduct next() {
                         return products.get(current++);
                     }
@@ -59,17 +62,21 @@ public class ProductList {
                     public void remove() {
                         throw new UnsupportedOperationException();
                     }
+
                     private int current = 0;
                 };
     }
 
+    /**
+     * Returns a list of products
+     * @return List of products
+     */
     public String formatProduct() {
         String items = "";
-        Iterator<LineProduct> iter = getProducts();
+        Iterator<LineProduct> iter = getItems();
         while (iter.hasNext())
             items += iter.next();
         return items;
     }
-
 
 }
