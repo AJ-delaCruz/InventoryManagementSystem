310914435cffd09fe7409d056fc7e9b93918b186
JesusM1997 <70827842+JesusM1997@users.noreply.github.com>
2020-12-05 23:38:43 -0800

    Add files via upload

---
+++ src/JUnitTest/EmployeeTest.java
@@ -0,0 +1,16 @@
+package JUnitTest;
+
+import Model.Employee;
+import junit.framework.TestCase;
+
+public class EmployeeTest extends TestCase {
+    public void test(){
+        Employee test = new Employee(12345, "password");
+        assertTrue(12345 == test.getAccountNumber());
+        assertTrue("password".equals(test.getPassword()));
+
+        Employee testTwo = new Employee(12345, "password");
+        assertTrue(test.equals(testTwo));
+
+    }
+}
